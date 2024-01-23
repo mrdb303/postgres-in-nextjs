7dd7b8fb249f9f1be386099ebad12c59fe523d2c
mrdb303 <dannybrien@ntlworld.com>
2024-01-23 15:57:08 +0000

    first commit

---
+++ src/app/posts/[id]/post.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default async function PostsPage({searchParams}) {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts"); // call the API
+  let posts = await response.json(); // parse the response as JSON
+
+  return (
+    <div>
+      <h1>Posts</h1>
+        {posts.map((post) => {
+          return (
+            <div key={post.id}>
+              <Link href={`/posts/${post.id}`}>{post.title}({post.userId})
+              </Link>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
