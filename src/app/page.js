4561fe715c74a501766df44177e91892b61e3943
mrdb303 <dannybrien@ntlworld.com>
2024-01-24 13:04:42 +0000

    another try

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,14 @@
 import { sql } from "@vercel/postgres";
 import styles from "./page.module.css";
 
-export default async function Home() {
+export default function Home() {
 
-  // const names = await sql`SELECT * FROM names`;
-  // let results = await response.json();
 
   return (
     <>
     <h1>Skeleton App</h1>
 
-        {/* {results.map((result) => {
-          return (
-            <>
-              <div key={result.id}>
-               <li>{'${result.name}} {result.age} {result.id}'}
-               </li>
-             </div> 
-            </>
-          );
-        })} */}
 
-        return (
-          <>
-          <p>Testing</p>
-          </>
-        );
         </>
   );
 }
